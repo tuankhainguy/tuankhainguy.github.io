5ea2d42547e9adcf7ff64c1752f43ca8a951d962
Khai Nguy <khaint0204@gmail.com>
2025-03-12 00:00:42 +1100

    feat: update layout

--- src/components/MainProjectContainer.vue
+++ src/components/MainProjectContainer.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
 import ProjectWrapper from './ProjectWrapper.vue';
 
 const { project } = defineProps<{
   project: string
 }>();
-
-const router = useRouter();
-
-const onBackButtonClick = () => {
-  router.push('/');
-}
 </script>
 
 <template>
   <div id="mainContainer">
-    <div class="topbar">
-      <button type="button" @click="onBackButtonClick">
-        <-
-      </button>
-    </div>
     <Suspense>
       <ProjectWrapper :project="project" />
       <template #fallback>
@@ -30,10 +18,4 @@ const onBackButtonClick = () => {
 </template>
 
 <style scoped>
-.topbar > button {
-  background-color: var(--accent);
-  color: var(--accent-compliment);
-  padding-block: 2px;
-  padding-inline: 12px;
-}
 </style>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
 import MainProjectContainer from '../components/MainProjectContainer.vue';
+import { useRouter } from 'vue-router';
 
 defineProps<{
   project: string
 }>();
+
+const router = useRouter();
+
+const onBackButtonClick = () => {
+  router.push('/');
+}
 </script>
 
 <template>
+  <div class="topbar">
+    <button type="button" @click="onBackButtonClick">
+      <-
+    </button>
+  </div>
   <MainProjectContainer :project="project" />
 </template>
 
 <style scoped>
+.topbar > button {
+  background-color: var(--accent);
+  color: var(--accent-compliment);
+  padding-block: 2px;
+  padding-inline: 12px;
+}
 </style>
