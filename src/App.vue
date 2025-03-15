2caa783796b60e39f3af42bb2dac3f30d35752f6
Khai Nguy <khaint0204@gmail.com>
2025-03-15 23:09:11 +1100

    feat: page transitions

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,23 @@
 </script>
 
 <template>
-  <RouterView />
+  <router-view v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <div class="wrapper" :key="$route.path">
+        <component :is="Component"/>
+      </div>
+    </Transition>
+  </router-view>
 </template>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
